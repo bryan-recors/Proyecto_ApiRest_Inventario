2c7ce57abb7af87aa611153bbbdb274ee13e4d59
aleandro <bryan-recors@hotmail.com>
2020-12-19 20:12:08 -0500

    creacion del modelo de proveedor

---
+++ apps/proveedores/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from apps.proveedores.models import Proveedor
+# Register your models here.
+class ProveedorAdmin(admin.ModelAdmin):
+    # que campos aparecen en el formulario si no lo pongo apareceran todos los del model
+    #fields = ('nombre','descripcion','telefono','celular','email','dias_visita')
+    #lo que se va a mostrar al listar
+    list_display = ('nombre','telefono','celular','email','dias_visita','created_at')
+    #buscador por nombre y dias de visita
+    search_fields = ('nombre','dias_visita')
+    #filtro
+    list_filter=('nombre','dias_visita','email')
+
+admin.site.register(Proveedor,ProveedorAdmin)
---
+++ apps/proveedores/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+# Create your models here.
+class Proveedor(models.Model):
+    nombre = models.CharField('Nombre del Producto',max_length=50)
+    descripcion = models.TextField('Descripcion',blank=True, null=True,)
+    telefono = models.CharField('Teléfono',max_length=10)
+    celular = models.CharField('Celular',max_length=10,blank=True, null=True)
+    email = models.EmailField('Correo',blank=True, null=True)
+    dias_visita = models.CharField('Dias de visita',max_length=300)
+    # para conocer cuanto una parroquias se Registro
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name= 'Proveedor'
+        verbose_name_plural = 'Proveedores'
+        ordering = ['nombre']
+    #para que se muestre por nombre en el admin
+    def __str__(self):
+        return self.nombre
