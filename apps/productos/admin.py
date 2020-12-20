4ef556cd25042273338a8b87e89401ceb9879cca
aleandro <bryan-recors@hotmail.com>
2020-12-19 20:31:43 -0500

    creacion del modelo de productos

--- apps/productos/admin.py
+++ apps/productos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from apps.productos.models import Producto
 # Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+    #lo que se va a mostrar al listar
+    list_display = ('nombre','precio','stock','proveedor','created_at')
+    #buscador por nombre y stock
+    search_fields = ('nombre','stock')
+    #filtro
+    list_filter=('nombre','precio','stock')
+
+admin.site.register(Producto,ProductoAdmin)
--- apps/productos/models.py
+++ apps/productos/models.py
@@ -1,2 +1,18 @@
 from django.db import models
+from apps.proveedores.models import Proveedor
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField('Nombre del Producto',max_length=50, unique=True)
+    descripcion = models.TextField('Descripcion',blank=True, null=True)
+    precio = models.FloatField('Precio de venta')
+    stock=models.IntegerField('Cantidad existente')
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE,verbose_name="Proveedor")
+    # para conocer cuanto una parroquias se Registro
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name= 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['nombre']
+    #para que se muestre por nombre en el admin
+    def __str__(self):
+        return self.nombre
