47fa8b9f311fee9d769d92b0b0ee31517257ce20
aleandro <bryan-recors@hotmail.com>
2020-12-23 13:52:25 -0500

    añadir al crud q cuando se liste el producto tambien se jale los datos del proveedor

--- apps/productos/serializers.py
+++ apps/productos/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from apps.productos.models import Producto
-
+#para que se liste todos los datos del proveedor lo exporto
+from apps.proveedores.serializers import ProveedorSerializers
 class ProductoSerializer(serializers.ModelSerializer):
+    #para que se liste los datos del proveedor tambien
+    proveedor = ProveedorSerializers(read_only = True)
     class Meta:
         model = Producto
         fields =('id','nombre','descripcion','precio','stock','proveedor')
