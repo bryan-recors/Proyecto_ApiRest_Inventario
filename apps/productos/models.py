eb53369710aa283fc01483faa1e81d2b95aed88a
Magaly <wendy.sarco3535@utc.edu.ec>
2020-12-27 10:40:32 -0500

    Validacion nombre y stock

--- apps/productos/models.py
+++ apps/productos/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from apps.proveedores.models import Proveedor
+
+from django.core import validators
+from django.core.exceptions import ValidationError
+
 # Create your models here.
 class Producto(models.Model):
     nombre = models.CharField('Nombre Producto',max_length=50, unique=True)
@@ -9,6 +13,17 @@ class Producto(models.Model):
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE,verbose_name="Proveedor")
     # para conocer cuanto una parroquias se Registro
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        if self.nombre.isdigit():
+            raise ValidationError('El campo "Nombre" no puede ser numerico')
+        else:
+            if self.stock < 0:
+                raise ValidationError('El campo "Cantidad Existente" no pueden ser un numero negativo')
+            else:
+                if self.stock ==0:
+                    raise ValidationError('El campo "Cantidad Existente" debe ser mayor a 1')
+
     class Meta:
         verbose_name= 'Producto'
         verbose_name_plural = 'Productos'
